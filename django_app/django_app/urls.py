24c152c3641a5ae93f272bb971cf5ffdcdfbaace
asharpc <40689302+asharpc@users.noreply.github.com>
2020-03-18 11:33:28 +0530

    add views into url

--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -17,9 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from .views import home, page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('page/', page),
+    
 ]
 
 if settings.DEBUG:
